a384e7979ac8cce0773544c5710d087ad3e8fb91
Ralto13 <doorumi13@gmail.com>
2025-03-24 22:19:13 +0900

    chore: 테일윈드 파일 v3 -> v4 형태로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-
-import { Geist, Geist_Mono } from 'next/font/google';
 import type { Metadata } from 'next';
-import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import '@/styles/global.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body>{children}</body>
     </html>
   );
 }
