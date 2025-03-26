0f6774ef689e8002a4d9fa13f1af5212c62fd4d3
Ralto13 <doorumi13@gmail.com>
2025-03-26 15:51:40 +0900

    [chore] 임시 저장

--- src/components/moleclues/Header.tsx
+++ src/components/moleclues/Header.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import Icon from '../atoms/icon/Icon';
 import Title from '../atoms/title/Title';
 
 const Header = () => {
+  const [isSearching, setIsSearching] = useState(false);
+  const [isNotichecking, setIsNotichecking] = useState(false);
+
   const onClickSearch = () => {
-    console.log('search');
+    setIsSearching(!isSearching);
   };
 
   const onClickNotification = () => {
-    console.log('notification');
+    setIsNotichecking(!isNotichecking);    
   };
 
   return (
     <div className="bg-primary01 flex w-[375px] justify-between px-[20px] pt-[23.5px] pb-[12.5px]">
       <Title />
       <div className="flex gap-[10px]">
-        <Icon iconName="bx bx-search-alt" className="text-white01" onClick={onClickSearch} />
-        <Icon iconName="bx bx-bell" className="text-white01" onClick={onClickNotification} />
+        {!isSearching && <Icon iconName="bx bx-search-alt" className="text-white01" onClick={onClickSearch} />}
+        {!isNotichecking && <Icon iconName="bx bx-bell" className="text-white01" onClick={onClickNotification} />}
       </div>
     </div>
   );
