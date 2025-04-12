d71a3494b8f06f838cfc2156b76ba53da2f6e753
Ralto13 <doorumi13@gmail.com>
2025-04-12 23:52:34 +0900

    style: bookmark 스토리 추가

---
+++ src/components/molecules/bookmark/Bookmark.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Bookmark from './Bookmark';
+
+const meta = {
+  title: 'molecules/bookmark',
+  component: Bookmark,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  render: (args) => <Bookmark {...args} />,
+} satisfies Meta<typeof Bookmark>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const isNotMarked: Story = {
+  args: {
+    isMarked: false,
+  },
+};
+
+export const isMarked: Story = {
+  args: {
+    isMarked: true,
+  },
+};
