f37221c8adbe41233ea177701ea435a3eb22d851
lillyYu <lilly@itdadev.com>
2025-05-20 23:43:46 +0900

    feat: 피드 작업 전 커밋

--- src/app/(services)/feed/page.tsx
+++ src/app/(services)/feed/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
+
+import Button from '@/components/atoms/button/Button';
 
 const FeedPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Link href="/feed/add-log">
+          <Button size="mini">일기 작성</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FeedPage;
