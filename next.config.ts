72e2a48ad704ae3c88f13410a8082198696b5948
lillyYu <lilly@itdadev.com>
2025-08-15 18:02:39 +0900

    feat: build testing #1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  // basePath: '/tablog-front',
   images: {
     domains: [
       'img.daisyui.com',
