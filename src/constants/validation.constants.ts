363f4f7fbf8e445c02bec0504164f1b21cb3c3bc
2hanbyeol1 <2hanbyeol1@naver.com>
2025-10-27 06:40:40 +0900

    feat: 채팅 websocket - 실시간 메시지 기능 추가

--- src/constants/validation.constants.ts
+++ src/constants/validation.constants.ts
@@ -32,3 +32,4 @@ export const STEP_FILE_REQUIRED = '조리 방법에 대한 이미지를 1~3개 �
 export const REPORT_REASON_REQUIRED = '신규 사유를 300자 이내로 입력해주세요.';
 export const REVIEW_CONTENT_REQUIRED = '리뷰를 300자 이내로 입력해주세요.';
 export const REVIEW_MEMO_REQUIRED = '개인 메모를 500자 이내로 읿력해주세요.';
+export const CHAT_MESSAGE_REQUIRED = '메시지를 입력해주세요.';
