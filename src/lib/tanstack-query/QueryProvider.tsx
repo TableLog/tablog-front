f3e758acea9cd0d87896ed1bbaf4dd2abd7e5aee
2hanbyeol1 <2hanbyeol1@naver.com>
2026-03-15 18:11:46 +0900

    feat: 에러 413 일 때 토스트 추가

--- src/lib/tanstack-query/QueryProvider.tsx
+++ src/lib/tanstack-query/QueryProvider.tsx
@@ -12,6 +12,16 @@ import axios from 'axios';
 
 import { showToast } from '@/utils/functions';
 
+const getErrorMessage = (status?: number) => {
+  switch (status) {
+    case 403:
+      return '해당 기능은 로그인 후 사용 가능합니다.';
+    case 413:
+      return '파일 크기가 너무 큽니다.';
+  }
+  return null;
+};
+
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(
     () =>
@@ -27,9 +37,11 @@ export const Provider = ({ children }: { children: React.ReactNode }) => {
         },
         mutationCache: new MutationCache({
           onError: (error) => {
-            if (axios.isAxiosError(error) && error.response?.status === 403) {
-              showToast({ message: '해당 기능은 로그인 후 사용 가능합니다.', type: 'error' });
-            }
+            if (!axios.isAxiosError(error)) return;
+
+            // 공통 에러 메시지 처리
+            const errorMessage = getErrorMessage(error.response?.status);
+            if (errorMessage) showToast({ message: errorMessage, type: 'error' });
           },
         }),
       }),
