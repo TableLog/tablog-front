a842b60fea452b7229c8acc4e8b132590ba548b9
2hanbyeol1 <2hanbyeol1@naver.com>
2026-03-15 20:16:44 +0900

    fix: 피드 댓글 무한스크롤로 변경

--- src/app/(services)/feed/comment/[id]/@item/feed-comment-list.tsx
+++ src/app/(services)/feed/comment/[id]/@item/feed-comment-list.tsx
@@ -6,6 +6,7 @@ import LoadingSpinner from '@/components/atoms/loading/LoadingSpinner';
 import ProfileImage from '@/components/atoms/profile-image/ProfileImage';
 import { Text } from '@/components/atoms/text/Text';
 import ChatInput from '@/components/molecules/chat/ChatInput';
+import InfiniteScroll from '@/components/organisms/infinite-scroll/InfiniteScroll';
 import { useGetCommentList, useGetCommentReplyList } from '@/hooks/queries/feed.hooks';
 import { ICommentResponse } from '@/types/api';
 import { convertDateFormat } from '@/utils/functions';
@@ -27,7 +28,12 @@ const FeedCommentList = ({ logId }: IFeedCommentListProps) => {
 
   return (
     <div>
-      <div className="flex flex-col gap-4">
+      <InfiniteScroll
+        className="flex flex-col gap-4"
+        hasNextPage={hasNextPage}
+        isFetching={isFetchingNextPage}
+        fetchNextPage={fetchNextPage}
+      >
         {commentList?.pages.map((page) =>
           page.data.boardComments.map((comment: ICommentResponse) => (
             <div key={comment.createdAt} className="flex items-start justify-between gap-2">
@@ -103,17 +109,7 @@ const FeedCommentList = ({ logId }: IFeedCommentListProps) => {
             </div>
           )),
         )}
-
-        {isFetchingNextPage && <LoadingSpinner />}
-      </div>
-
-      {hasNextPage && (
-        <div className="flex items-center justify-center py-6">
-          <Button size="mini" buttonColor="grey04" onClick={() => fetchNextPage()}>
-            댓글 더 보기
-          </Button>
-        </div>
-      )}
+      </InfiniteScroll>
     </div>
   );
 };
