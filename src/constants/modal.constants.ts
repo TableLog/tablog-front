eb9d00cb8332924654e324bb968c280f5c43beb5
lillyYu <lilly@itdadev.com>
2026-03-20 20:28:38 +0900

    fix: 회원 탈퇴 30일 경고 알러트

--- src/constants/modal.constants.ts
+++ src/constants/modal.constants.ts
@@ -2,5 +2,6 @@ export const DELETE_FEED_MODAL = 'delete_feed';
 export const DELETE_RECIPE_MODAL = 'delete-recipe';
 export const PAY_RECIPE_MODAL = 'pay-recipe';
 export const NO_RECIPE_MODAL = 'no-recipe';
+export const UNREGISTER_MODAL = 'unregister';
 export const DELETE_REVIEW_MODAL = 'delete-review';
 export const LOGIN_REQUIRED_MODAL = 'login-required';
